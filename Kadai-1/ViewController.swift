b9c31190da9cca9f0fc6a40feeb399ed14ab0101
Shoko <shsuzu1992@gmail.com>
2021-11-02 22:44:48 +0900

    Fixed: var name

--- Kadai-1/ViewController.swift
+++ Kadai-1/ViewController.swift
@@ -1,19 +1,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet private weak var label1: UITextField!
-    @IBOutlet private weak var label2: UITextField!
-    @IBOutlet private weak var label3: UITextField!
-    @IBOutlet private weak var label4: UITextField!
-    @IBOutlet private weak var label5: UITextField!
-    @IBOutlet private weak var total: UILabel!
+    @IBOutlet private weak var textField1: UITextField!
+    @IBOutlet private weak var textField2: UITextField!
+    @IBOutlet private weak var textField3: UITextField!
+    @IBOutlet private weak var textField4: UITextField!
+    @IBOutlet private weak var textField5: UITextField!
+    @IBOutlet private weak var totalLabel: UILabel!
     @IBAction private func buttonTapped(_ sender: Any) {
-        let num1 = Int(label1.text ?? "") ?? 0
-        let num2 = Int(label2.text ?? "") ?? 0
-        let num3 = Int(label3.text ?? "") ?? 0
-        let num4 = Int(label4.text ?? "") ?? 0
-        let num5 = Int(label5.text ?? "") ?? 0
+        let num1 = Int(textField1.text ?? "") ?? 0
+        let num2 = Int(textField2.text ?? "") ?? 0
+        let num3 = Int(textField3.text ?? "") ?? 0
+        let num4 = Int(textField4.text ?? "") ?? 0
+        let num5 = Int(textField5.text ?? "") ?? 0
         let totalNum = num1 + num2 + num3 + num4 + num5
-        total.text = String(totalNum)
+        totalLabel.text = String(totalNum)
     }
 }
